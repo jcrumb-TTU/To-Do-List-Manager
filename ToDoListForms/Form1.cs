0c73cbe6e270ea28cfb5e27730759572e4a46ecf
jcrumb-TTU <jcrumb42@tntech.edu>
2025-07-09 12:36:16 -0500

    Added code to connect to database and read input from login

--- ToDoListForms/Form1.cs
+++ ToDoListForms/Form1.cs
@@ -1,10 +1,25 @@
+using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 namespace ToDoListForms
 {
     public partial class Form1 : Form
     {
+        string dbConnection = "Server=localhost;Database=todomanager;Uid=root;pwd=examplepassword"
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string userName = userNameBox.Text;
+            string password = passwordBox.Text;
+            
+            //debugging statement
+            //MessageBox.Show("You entered " + userName + " and " + password);
         }
     }
 }
