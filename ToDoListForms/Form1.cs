f0e9b9d21fa10b97e437666317fa2704891134f2
jcrumb-TTU <jcrumb42@tntech.edu>
2025-07-09 12:44:18 -0500

    Added Register button event and made input variables more modular

--- ToDoListForms/Form1.cs
+++ ToDoListForms/Form1.cs
@@ -7,7 +7,12 @@ namespace ToDoListForms
     public partial class Form1 : Form
     {
         //string connection, source of format from https://learn.microsoft.com/en-us/windows/apps/develop/data-access/mysql-database
-        const string dbConnection = "Server=localhost;Database=todomanager;Uid=root;pwd=examplepassword"
+        const string dbConnection = "Server=localhost;Database=todomanager;Uid=root;pwd=examplepassword";
+
+        string userName;
+        string password;
+        string firstName;
+        string lastName;
 
         public Form1()
         {
@@ -16,11 +21,19 @@ namespace ToDoListForms
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            string userName = userNameBox.Text;
-            string password = passwordBox.Text;
-            
+            userName = userNameBox.Text;
+            password = passwordBox.Text;
+
             //debugging statement
             //MessageBox.Show("You entered " + userName + " and " + password);
+        }
+
+        private void regButton_Click(object sender, EventArgs e)
+        {
+            firstName = firstNameBox.Text;
+            lastName = lastNameBox.Text;
+            userName = userNameRegBox.Text;
+            password = passwordRegBox.Text;
         }
     }
 }
